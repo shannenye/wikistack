0ce8561269840165627824de03081edf4d603a1c
Shannen <shannen.ye@gmail.com>
2017-11-10 16:43:38 -0500

    wikistack

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extend: true}));
+app.use(bodyParser.json());
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+app.listen(3000, () => {console.log('Listening on 3000')});
